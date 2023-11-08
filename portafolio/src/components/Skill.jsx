cad57123ef0425b91312c897d65e86f78ab86d7c
Camilop4 <jcrp_2005@hotmail.com>
2023-11-08 14:56:06 -0500

    update skills

---
+++ portafolio/src/components/Skill.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styleSkill from "./Skill.module.css";
+import {FaRegCheckCircle, FaHtml5, FaReact } from "react-icons/fa";
+import {FaCss3Alt, FaAngular} from "react-icons/fa6";
+import {BiLogoPostgresql, BiLogoMongodb, BiLogoRedux} from "react-icons/bi";
+import {SiSequelize} from "react-icons/si";
+import {DiJavascript} from "react-icons/di";
+import {AiFillGithub} from "react-icons/ai";
+import {TbBrandNodejs} from "react-icons/tb";
+
+
+
+
+const Skill = () => {
+    return (
+        <div className={styleSkill.Skill}>
+            <h1>Skills</h1>
+
+            <div className={styleSkill.Soft}>
+    
+                <h2>SOFT SKILLS</h2>
+                
+                    <FaRegCheckCircle />Exelente comunicacion  <FaRegCheckCircle /> Buen trabajo en equipo  <FaRegCheckCircle />Capacidad de adaptacion  <FaRegCheckCircle />Educacion constante  <FaRegCheckCircle />Proactividad     
+                
+            </div>
+
+            <div className={styleSkill.Hard}>
+                <h2>HARD SKILLS</h2>
+                
+                
+                    <FaCss3Alt className={styleSkill.Icon}/>  <FaAngular className={styleSkill.Icon}/> <FaHtml5 className={styleSkill.Icon}/> <FaReact className={styleSkill.Icon}/> <BiLogoPostgresql className={styleSkill.Icon}/> <BiLogoMongodb className={styleSkill.Icon}/> <BiLogoRedux className={styleSkill.Icon}/> <SiSequelize className={styleSkill.Icon}/> <AiFillGithub className={styleSkill.Icon}/> <TbBrandNodejs className={styleSkill.Icon}/> <DiJavascript className={styleSkill.Icon}/>
+
+
+
+
+
+
+            </div>
+        </div>
+    )
+}
+
+
+export default Skill;
